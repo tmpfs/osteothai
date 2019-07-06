dbb037a2b634a54fb12be46f832b529f680f52e2
muji <muji@tmpfs.org>
2019-07-06 11:53:21 +0800

    Adding events listings.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,17 @@
 class Application {
+
+  constructor() {
+    const moreLink = document.querySelector('[href="#read-more"]');
+    moreLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      const el = document.querySelector('.more');
+      el.classList.remove('hidden');
+      el.classList.add('block');
+      e.currentTarget.classList.add('hidden');
+    });
+
+  }
+
 }
 
 module.exports = Application
